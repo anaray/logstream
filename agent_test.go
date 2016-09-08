02630ae7b0836d65f72e8fe8c41b8b292e40cb8e
anumodh <anumodh@gmail.com>
2016-09-07 23:25:29 -0700

    adding agent

---
+++ agent_test.go
@@ -0,0 +1,11 @@
+package logstream
+
+import(
+  "testing"
+  "time"
+)
+
+func TestAgent(t *testing.T)  {
+  agent := NewAgent(time.Minute,time.Duration(30)*time.Second)
+  agent.Start("testdir/logs/opendirectoryd.log",0)
+}
