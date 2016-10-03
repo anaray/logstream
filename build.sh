fa30720c851d6114a12f244959d92c9b60478178
anumodh <anumodh@gmail.com>
2016-10-03 10:59:29 -0700

    updating build scrit

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ rm -f ../../bin/service_*
 TARGET=$1
 go get github.com/mitchellh/gox
 if [ -z $target ]; then
-  TARGET="linux/amd64"
+  gox -osarch=$TARGET -output "../../bin/logstream_{{.OS}}_{{.Arch}}" logstream/service
 fi
-gox -osarch=$TARGET -output "../../bin/logstream_{{.OS}}_{{.Arch}}" logstream/service
