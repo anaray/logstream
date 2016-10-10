c645f76b1b535ec31745437d0349b3651ae8fa41
anumodh <anumodh@gmail.com>
2016-10-10 11:15:52 -0700

    updating build.sh with glide update

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #rm -f ../../bin/service_*
 TARGET=$1
+glide update
 go get github.com/mitchellh/gox
 if [ -z $target ]; then
   gox -osarch=$TARGET -output "$GOPATH/bin/logstream_{{.OS}}_{{.Arch}}" github.com/anaray/logstream/main
